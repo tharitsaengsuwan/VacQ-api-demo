0e538f6dc6d182323bd14a0e10a219c9bdd3f5f4
tharit <tharit.pts@gmail.com>
2022-04-07 14:05:36 +0700

    add swagger api doc

--- server.js
+++ server.js
@@ -11,12 +11,33 @@ const xss = require("xss-clean");
 const rateLimit = require("express-rate-limit");
 const hpp = require("hpp");
 const cors = require('cors');
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUI = require('swagger-ui-express')
 
 dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
 const app = express();
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1'
+      }
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 //Body parser
 app.use(express.json());
